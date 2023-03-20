5c9020e5dbfcf5acc84cc8b18e9f61208391e7ed
cuifch <cuifch@neusoft.com>
2023-03-20 10:10:16 +0800

    提交院感, 修正ServiceFactory命名

--- src/main/java/com/hospital/middleware/hospitalinfection/webservice/HosInfWebServerImpl.java
+++ src/main/java/com/hospital/middleware/hospitalinfection/webservice/HosInfWebServerImpl.java
@@ -3,7 +3,7 @@ package com.hospital.middleware.hospitalinfection.webservice;
 import javax.jws.WebService;
 import java.util.Map;
 
-import com.hospital.middleware.hospitalinfection.service.ServiceFactory;
+import com.hospital.middleware.hospitalinfection.service.HIServiceFactory;
 import org.json.JSONObject;
 import org.json.XML;
 import org.slf4j.Logger;
@@ -15,17 +15,17 @@ import org.springframework.stereotype.Service;
 @WebService
 public class HosInfWebServerImpl implements HosInfWebServer {
     @Autowired
-    private ServiceFactory sf;
+    private HIServiceFactory sf;
+    private Logger log = LoggerFactory.getLogger(HosInfWebServerImpl.class);
 
     @Override
     public String acceptMessage(String message) {
-        System.out.println("传入的参数(XML):\n" + message);
+        log.info("传入的参数(XML):\n" + message);
         JSONObject json = XML.toJSONObject(message);
         String jsonString = json.toString();
-        System.out.println("传入的参数(JSON):\n" +jsonString);
+        log.info("传入的参数(JSON):\n" +jsonString);
         Map param = (Map) json.toMap();
         String xmlResult = sf.getXMLData(param);
-
 
         return xmlResult;
     }
