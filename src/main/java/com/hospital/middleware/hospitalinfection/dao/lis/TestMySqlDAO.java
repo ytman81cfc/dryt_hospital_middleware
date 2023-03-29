11391c3f5b2a5b0b8bf7e196a256baf3f17d093f
cuifch <cuifch@neusoft.com>
2023-03-29 10:21:28 +0800

    LIS库测试

---
+++ src/main/java/com/hospital/middleware/hospitalinfection/dao/lis/TestMySqlDAO.java
@@ -0,0 +1,8 @@
+package com.hospital.middleware.hospitalinfection.dao.lis;
+
+import java.util.List;
+import java.util.Map;
+
+public interface TestMySqlDAO {
+    List<Map> queryCurrentDateTime();
+}
