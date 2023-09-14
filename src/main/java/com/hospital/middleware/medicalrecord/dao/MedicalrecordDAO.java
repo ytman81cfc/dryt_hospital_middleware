782a3d7611e1a36f5c7e21f81e63ace6b7ba0b3e
congrh <congrh@neusoft.com>
2023-09-14 16:54:54 +0800

    病案

--- src/main/java/com/hospital/middleware/medicalrecord/dao/MedicalrecordDAO.java
+++ src/main/java/com/hospital/middleware/medicalrecord/dao/MedicalrecordDAO.java
@@ -3,10 +3,14 @@ package com.hospital.middleware.medicalrecord.dao;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Map;
 
 @Mapper
 public interface MedicalrecordDAO {
-    Map selectInid(@Param("map")Map paramMap);
+    Map  queryMedcard(String orgcode,String inid  );
 
+    List<Map> queryIcde(String orgcode,String inid );
+
+    List<Map> queryOper(String orgcode,String inid );
 }
