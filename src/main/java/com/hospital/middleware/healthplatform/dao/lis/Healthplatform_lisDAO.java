071a6e7179573399183f7bb18a60a0fff243be08
congrh <congrh@neusoft.com>
2023-12-05 21:56:08 +0800

    健康平台

--- src/main/java/com/hospital/middleware/healthplatform/dao/lis/Healthplatform_lisDAO.java
+++ src/main/java/com/hospital/middleware/healthplatform/dao/lis/Healthplatform_lisDAO.java
@@ -2,7 +2,12 @@ package com.hospital.middleware.healthplatform.dao.lis;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface Healthplatform_lisDAO {
 
+    //
+    List<Map> queryJysfxmb_lis(String orgcode, String begtime, String endtime);
 }
