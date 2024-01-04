774e2e58a9be03d75b5a5dad438fd83529f64bc4
congrh <congrh@neusoft.com>
2024-01-04 11:06:16 +0800

    卫健接口

--- src/main/java/com/hospital/middleware/HmApplication.java
+++ src/main/java/com/hospital/middleware/HmApplication.java
@@ -12,10 +12,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class HmApplication {
     public static void main(String[] args) {
 
-         ConfigurableApplicationContext cac = SpringApplication.run(HmApplication.class, args);
+         /*ConfigurableApplicationContext cac = SpringApplication.run(HmApplication.class, args);
         HpService hp = cac.getBean(HpService.class);
         hp.queryData("2023-12-01", "2023-12-28", "EMR_RCBCJL");
-        System.out.println("");
+        System.out.println("");*/
         SpringApplication.run(HmApplication.class, args);
     }
 }
